b88470382b6fd65495242a7a0e21794ea72a55b7
Pedro <pedroprocuroemprego@gmail.com>
2025-04-23 21:54:50 -0300

    Desafio basico

--- conta_banco/src/ContaTerminal.java
+++ conta_banco/src/ContaTerminal.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Scanner entrada= new Scanner(System.in);
+        int numeroAgencia;
+        String nome,agencia;
+        double saldo;
+
+
+        System.out.printf("Por Favor,Informe o Nome da Sua agencia :");
+        agencia =entrada.next();
+
+        System.out.println("informe o numero da sua agencia: ");
+        numeroAgencia=entrada.nextInt();
+
+        System.out.println("Insira o seu Nome :");
+        nome=entrada.next();
+
+        System.out.println("Digite o seu saldo :");
+        saldo=entrada.nextDouble();
+
+
+
+        System.out.println(" Olá "+nome+" obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numeroAgencia+" e seu saldo "+saldo+" já está disponível para saque");
+
+        
+        
+
+
+
+         
+
+
+
+
     }
 }
